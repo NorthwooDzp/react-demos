5ea59b945b955bf7f90d93a815f645648668f1f2
Volodymyr L <northwoodzp@gmail.com>
2026-01-07 12:07:08 +0000

    https://www.youtube.com/watch?v=M9O5AjEFzKw&list=PLSDeUiTMfxW6nMcmZPUG4SgFPAlazWB_S
    55:50
    
    lists and arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import Header from './components/Header';
+/*import Header from './components/Header';
 import Footer from './components/Footer.tsx';
-import MainContent from './components/MainContent.tsx';
+import MainContent from './components/MainContent.tsx';*/
 
 const App = () => {
+  const list: number[] = [1, 2, 3, 4, 5];
+
   return (
     <>
-      <Header />
+      <ul>
+        {list.map((el) => (
+          <li key={el}>{el}</li>
+        ))}
+      </ul>
+      {/*<Header />
       <MainContent />
-      <Footer />
+      <Footer />*/}
     </>
   );
 };
