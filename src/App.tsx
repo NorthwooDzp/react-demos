19155cc66ca61fe449b96c32f4ff0b9758c3721a
Volodymyr L <northwoodzp@gmail.com>
2026-01-07 12:35:08 +0000

    https://www.youtube.com/watch?v=M9O5AjEFzKw&list=PLSDeUiTMfxW6nMcmZPUG4SgFPAlazWB_S
    01:03:00
    
    lists and arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import UsersList from './components/UsersList.tsx';
+import ProductsList from './components/ProductsList.tsx';
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <UsersList />
+      <ProductsList />
+    </>
+  );
 };
 
 export default App;
