b0cd8f88aa24fd1017f78e602b41d77d671eef15
Volodymyr L <northwoodzp@gmail.com>
2026-01-08 08:23:32 +0000

    React Tutorial Full Course - start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { useState } from 'react';
-
 const App = () => {
-  const [counter, setCounter] = useState<number>(0);
-
-  function increaseCount(): void {
-    setCounter(counter + 1);
-  }
-
-  setTimeout(() => {
-    setCounter(40);
-  }, 5000);
-
-  return (
-    <>
-      <h1>{counter}</h1>
-      <button type="button" onClick={increaseCount}>
-        Increase count
-      </button>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
